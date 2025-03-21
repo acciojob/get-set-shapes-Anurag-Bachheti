47bb814a0b4f48afb8ae2df3b1c6599c47dde65a
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-03-21 17:01:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,61 @@
-//complete this code
-class Rectangle {}
 
-class Square extends Animal {}
+class Rectangle{
+    constructor(width, height){
+        this._width = width;
+        this._height= height;
+    }
+    get width(){
+        return this._width;
+    }
+    set width(newWidth){
+        if(width > 0){
+            this._width = newWidth
+        } else{
+            return 0;
+        }
+    }
+
+    get height(){
+        return this._height;
+    }
+
+    set height(newHeight){
+        if(height > 0){
+            this._height = newHeight
+        } else{
+            return 0;
+        }
+    }
+
+    getArea(){
+        return this._width * this._height;
+    }
+}
+
+class Square extends Rectangle{
+    constructor(side){
+        super(side, side)
+        this._side = side
+    }
+
+    get side(){
+        this._side;
+    }
+    
+    set side(newSide){
+        if(newSide>0){
+            this._side = newSide;
+            this._width = newSide;
+            this._height = newSide;
+        } else {
+            console.log('side length cannot be 0')
+        }
+    }
+
+    getPerimeter(){
+        return 4 * this._side
+    }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
